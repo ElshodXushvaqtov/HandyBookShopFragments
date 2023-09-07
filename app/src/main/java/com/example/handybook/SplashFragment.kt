8cf6e1307b425688b5f798c858f25b465facb658
User_4 <elshodxushvaqtov07@gmail.com>
2023-09-07 13:20:38 +0500

    BoshSahifa

--- app/src/main/java/com/example/handybook/SplashFragment.kt
+++ app/src/main/java/com/example/handybook/SplashFragment.kt
@@ -1,21 +1,19 @@
 package com.example.handybook
 
-import android.annotation.SuppressLint
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import com.example.handybook.databinding.FragmentSplashScreenBinding
-
+import com.example.handybook.databinding.FragmentSplashBinding
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
-
-@SuppressLint("CustomSplashScreen")
-class SplashScreen : Fragment() {
-    lateinit var binding: FragmentSplashScreenBinding
+class SplashFragment : Fragment() {
+    lateinit var binding: FragmentSplashBinding
     private var param1: String? = null
     private var param2: String? = null
 
@@ -31,20 +29,17 @@ class SplashScreen : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentSplashScreenBinding.inflate(layoutInflater)
-        binding.kirishBtn.setOnClickListener {
-            findNavController().navigate(R.id.action_splashScreen_to_loginFragment)
-        }
-        binding.royhatdanOtishTxt.setOnClickListener {
-            findNavController().navigate(R.id.action_splashScreen_to_registrationFragment)
-        }
+        binding=FragmentSplashBinding.inflate(layoutInflater)
+Handler(Looper.getMainLooper()).postDelayed({
+findNavController().navigate(R.id.action_splashFragment_to_splashScreen)
+},2500)
         return binding.root
     }
 
     companion object {
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            SplashScreen().apply {
+            SplashFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
